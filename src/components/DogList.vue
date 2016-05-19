29d8dcd800c620a59e07b131a67fabca18ffd7f0
RicciFlowing <benjamin@mathe-sellin.de>
2016-05-19 20:48:10 +0200

    Styled search form

--- src/components/DogList.vue
+++ src/components/DogList.vue
@@ -1,5 +1,5 @@
 <template>
-  <input class="search-form"v-model="search">
+  <input class="search-form" v-model="search" placeholder="Search">
   <ul>
     <dog v-for="dog in dogs| filterBy search" :dog="dog"> </dog>
   </ul>
@@ -13,6 +13,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.search-form{
+  display: block;
+  border: none;
+  border-bottom: solid 1px;
+  height: 20px;
+  width: 200px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+}
 ul {
   list-style: none;
   display: flex;
