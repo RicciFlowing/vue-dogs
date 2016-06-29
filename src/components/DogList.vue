80e826ee28d17926de34f11236806bead8e0a6e0
RicciFlowing <benjamin@mathe-sellin.de>
2016-06-29 19:47:27 +0200

    Inline beginner friendly checkbox

--- src/components/DogList.vue
+++ src/components/DogList.vue
@@ -1,7 +1,10 @@
 <template>
-  <input class="search-form" v-model="search" placeholder="Search">
-  <input type="checkbox" id="checkbox" v-model="beginner">
-  <label for="checkbox"> beginner friendly</label>
+  <div class="input-group">
+    <input class="search-form" v-model="search" placeholder="Search">
+    <input type="checkbox" id="beginner-checkbox" v-model="beginner">
+    <label for="checkbox"> beginner friendly</label>
+  </div>
+
   <ul>
     <dog v-for="dog in dogs| filterBy true in 'pinned'" :dog="dog"> </dog>
     <dog v-for="dog in dogs| filterBy false in 'pinned' | filterBy search | filterBy beginner||null in 'beginner'" :dog="dog"> </dog>
@@ -16,16 +19,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.input-group {
+  text-align: center;
+}
+
+
 .search-form{
-  display: block;
+  display: inline-block;
   border: none;
   border-radius: 2px;
   border-bottom: solid 1px;
   height: 30px;
   width: 200px;
   text-align: center;
-  margin-left: auto;
-  margin-right: auto;
+  margin-right: 20px;
 }
 ul {
   list-style: none;
