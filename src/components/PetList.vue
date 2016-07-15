52946aaa9c93865553bad38d1804a4513aa4a300
RicciFlowing <benjamin@mathe-sellin.de>
2016-07-15 15:40:19 +0200

    Added petList component

---
+++ src/components/PetList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="input-group">
+    <input class="search-form" v-model="search" placeholder="Search">
+    <div>
+      <input type="checkbox" id="beginner-checkbox" v-model="beginner">
+      <label for="checkbox"> beginner friendly</label>
+    </div>
+  </div>
+
+  <ul>
+    <dog v-for="pet in pets| filterBy true in 'pinned'" :dog="dog"> </dog>
+    <dog v-for="pet in pets| filterBy false in 'pinned' | filterBy search | filterBy beginner||null in 'beginner'" :dog="dog"> </dog>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['pets'],
+  data() { return {
+      search: '',
+      beginner: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.input-group {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+
+
+.search-form{
+
+  border: none;
+  border-radius: 2px;
+  border-bottom: solid 1px;
+  height: 30px;
+  width: 200px;
+  text-align: center;
+  margin-right: 20px;
+}
+
+ul {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+@media (max-width: 600px) {
+  ul {
+    width: 100%;
+    list-style: none;
+    justify-content: center;
+  }
+
+  .input-group {
+    flex-direction: column;
+    align-items: center;
+  }
+ }
+
+</style>
