0e1bb23142faf46ce3d9b0df1a0656aa79f8f171
RicciFlowing <benjamin@mathe-sellin.de>
2016-07-22 19:39:12 +0200

    Cleaned the code of unused references

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import Vue from 'vue'
-// import Dog from './components/Dog.vue'
-// Vue.component('dog',Dog);
-// import DogList from './components/DogList.vue'
-// Vue.component('dog-list',DogList);
 import Pet from './components/Pet.vue'
 Vue.component('pet',Pet);
 import PetList from './components/PetList.vue'
